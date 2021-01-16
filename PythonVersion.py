7b2f064106cfd2d25cc8d56296f27650bbd94ed1
David <nzdr@outlook.com>
2021-01-16 03:57:44 +0000

    Outputs UserId for user with valid credentials

--- PythonVersion.py
+++ PythonVersion.py
@@ -2,10 +2,11 @@ import sys
 print(sys.version)
 
 import boto3
+user=()
+
 sts = boto3.client('sts')
 try:
-    sts.get_caller_identity()
-    print("Credentials are valid.")
+    user=sts.get_caller_identity()['UserId']
+    print('The users userId is {0}'.format(user))
 except boto3.exceptions.ClientError:
     print("Credentials are NOT valid.")
-    
